fe5b2e66d754c3314a28c79c91843f9d3e09a774
Alexandra <alexandracutipa@upeu.edu.pe>
2019-04-11 21:19:06 -0500

    asdf

---
+++ script.js
@@ -0,0 +1,35 @@
+function inicio() {
+	document.getElementById("aa1").style.display= "block";
+	document.getElementById("aa2").style.display= "none";
+	document.getElementById("aa3").style.display= "none";
+	document.getElementById("aa4").style.display= "none";
+	document.getElementById("aa5").style.display= "none";
+}
+function ventas() {
+	document.getElementById("aa1").style.display= "none";
+	document.getElementById("aa2").style.display= "block";
+	document.getElementById("aa3").style.display= "none";
+	document.getElementById("aa4").style.display= "none";
+	document.getElementById("aa5").style.display= "none";
+}
+function reportes() {
+	document.getElementById("aa1").style.display= "none";
+	document.getElementById("aa2").style.display= "none";
+	document.getElementById("aa3").style.display= "block";
+	document.getElementById("aa4").style.display= "none";
+	document.getElementById("aa5").style.display= "none";
+}
+function clientes() {
+	document.getElementById("aa1").style.display= "none";
+	document.getElementById("aa2").style.display= "none";
+	document.getElementById("aa3").style.display= "none";
+	document.getElementById("aa4").style.display= "block";
+	document.getElementById("aa5").style.display= "none";
+}
+function productos() {
+	document.getElementById("aa1").style.display= "none";
+	document.getElementById("aa2").style.display= "none";
+	document.getElementById("aa3").style.display= "none";
+	document.getElementById("aa4").style.display= "none";
+	document.getElementById("aa5").style.display= "block";
+}
